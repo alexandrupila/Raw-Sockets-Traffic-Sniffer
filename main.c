b552c0dbe7f28cdcda933eb0a3a291ddf7cf6937
alexandrupila <alexandrupila@gmail.com>
2024-06-19 13:40:53 +0300

    afisarea a cateva tipuri de pachete si date despre acestea

--- main.c
+++ main.c
@@ -9,27 +9,95 @@
 #include <arpa/inet.h>
 #include <net/ethernet.h>
 
-void processPacket(unsigned char* buffer, int size)
-{
-    struct iphdr *iph=(struct iphdr*) buffer;
+void processPacket(unsigned char* buffer, int size,FILE* logfile)
+{   
+    struct ethhdr* eth=(struct ethhdr*)(buffer);
+
+    if(htons(eth->h_proto)!=0x800) return;
+
+    struct iphdr *iph=(struct iphdr*) (buffer+sizeof(struct ethhdr));
     switch(iph->protocol)
     {
         case 1:
             printf("ICMP Packet received\n");
+            printICMP(buffer,size,logfile);
             break;
         case 2:
             printf("IGMP Packet received\n");
             break;
         case 6:
             printf("TCP Packet received\n");
+            printTCP(buffer,size,logfile);
             break;
         case 17:
             printf("UDP Packet received\n");
+            printUDP(buffer,size,logfile);
             break;
         default:
             printf("Currently Unknown Packet received\n");
             break;
     }
+}
+
+void printTCP(unsigned char* buffer, int size,FILE* logfile)
+{       
+
+    unsigned short iphdrlen;
+    struct iphdr* iph=(struct iphdr*) buffer;
+    iphdrlen=iph->ihl*4;
+
+    struct tcphdr* tcph=(struct tcphdr*)(buffer+iphdrlen+sizeof(struct ethhdr));
+
+    fprintf(logfile,"TCP RECEIVED\n");
+
+    fprintf(logfile,"Source port:%u Destination port:%u Seq. number:%u Ack seq:%u\n",tcph->source,tcph->dest,tcph->seq,tcph->ack_seq);
+    fprintf(logfile,"FLAGS: ");
+    if((int)tcph->urg) fprintf(logfile,"URG ");
+    if((int)tcph->ack) fprintf(logfile,"ACK ");
+    if((int)tcph->psh) fprintf(logfile,"PSH ");
+    if((int)tcph->rst) fprintf(logfile,"RST ");
+    if((int)tcph->syn) fprintf(logfile,"SYN ");
+    if((int)tcph->fin) fprintf(logfile,"FIN ");
+    fprintf(logfile,"\n");
+    fprintf(logfile,"Window: %d\n",tcph->window);
+    fprintf(logfile,"Checksum: %d\n",tcph->check);
+    fprintf(logfile,"Urgent Pointer: %d\n",tcph->urg_ptr);
+
+}
+
+void printUDP(unsigned char* buffer, int size, FILE* logfile)
+{
+    unsigned short iphdrlen;
+    struct iphdr* iph=(struct iphdr*)buffer;
+    iphdrlen=iph->ihl*4;
+
+    struct udphdr* udph=(struct udphdr*)(buffer+iphdrlen+sizeof(struct ethhdr));
+
+    fprintf(logfile,"UDP RECEIVED\n");
+
+    fprintf(logfile,"Source port:%u Destination port:%u Length:%u Checksum:%u\n",udph->source,udph->dest,udph->len,udph->check);
+
+}
+
+void printICMP(unsigned char* buffer, int size, FILE* logfile)
+{
+    unsigned short iphdrlen;
+    struct iphdr* iph=(struct iphdr*) buffer;
+    iphdrlen=iph->ihl*4;
+
+    struct icmphdr* icmph=(struct icmphdr*) buffer;
+    
+    fprintf(logfile,"ICMP RECEIVED\n");
+
+    fprintf(logfile,"Type: %d ",(unsigned int)icmph->type);
+
+    if((unsigned int)icmph->type==11) fprintf(logfile," TTL EXPIRED\n");
+    else if ((unsigned int)icmph->type==ICMP_ECHOREPLY) fprintf(logfile," ICMP ECHO REPLY\n");
+    else fprintf(logfile,"\n");
+
+    fprintf(logfile,"Code: %d\n",(unsigned int)icmph->code);
+    fprintf(logfile,"Checksum: %d\n",(unsigned int)icmph->checksum);
+
 }
 
 int main()
@@ -44,13 +112,14 @@ int main()
 
     printf("Traffic sniffer starting\n");
 
-    int sock_raw =socket(AF_INET,SOCK_RAW,IPPROTO_TCP);
+    int sock_raw =socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
 
     if(sock_raw<0)
     {
         printf("Socket error\n");
         return 1;
     }
+    int packet_nr=0;
     while(1)
     {
         saddr_size=sizeof(saddr);
@@ -60,7 +129,9 @@ int main()
             printf("Error when receiving packets");
             return 1;
         }
-        processPacket(buffer,data_size);
+        processPacket(buffer,data_size,logfile);
+        packet_nr++;
+        if(packet_nr==5000) exit(0);
 
     }
 
